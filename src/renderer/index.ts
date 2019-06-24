cfcebeb022eb4e888afb649090695985a1a9c8a7
Yinzhe_Qi <qiyzh@neusoft.com>
2019-06-24 15:27:15 +0800

    init

---
+++ src/renderer/index.ts
@@ -0,0 +1,28 @@
+import './renderer.sass'
+import * as THREE from 'three'
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+const root = document.getElementById('app') as HTMLElement
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(root.clientWidth, root.clientHeight)
+root.appendChild(renderer.domElement)
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+camera.position.z = 5
+
+function animate() {
+  requestAnimationFrame(animate)
+
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+
+  renderer.render(scene, camera)
+}
+
+animate()
